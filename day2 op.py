d6aaed3b23e8a5c2992086f3d88536dccbf1975b
Sheshankk <85849485+Sheshankk@users.noreply.github.com>
2021-06-15 03:36:48 +0530

    Add files via upload

---
+++ day2 op.py
@@ -0,0 +1,18 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:/Users/Home/OneDrive/Documents/day2 task.py ============
+30 days 30 hours challenge
+thirty
+y
+will
+10
+i will win
+30 days30 hours
+30 days 30 hours
+thirty days thirty hours
+Don't trouble trouble until trouble troubles you
+22
+False
+False
+>>> 
---
+++ day2 task.py
@@ -0,0 +1,58 @@
+#How to print a value
+print('30 days 30 hours challenge')
+
+#Assigning String to Variable
+Hours = "thirty"
+print(Hours)
+
+#Indexing using String
+days="thirty days"
+print(days[5])
+
+#How to print the particular character from certain text
+challenge="i will win"
+print(challenge[2:6])
+
+#Print the length of Character
+challenge="i will win"
+print(len(challenge))
+
+#Convert String into lower character
+challenge="I WILL WIN"
+print(challenge.lower())
+
+#String Concatenation – Joining two strings
+a="30 days"
+b="30 hours"
+c= a+b
+print(c)
+
+#Adding space during concatenation
+a="30 days"
+b="30 hours"
+c= a+' '+b
+print(c)
+
+#casefold() - Usage
+text="thirty days thirty hours"
+x=text.casefold()
+print(x)
+
+text="don't trouble trouble until trouble troubles you"
+y=text.capitalize()
+print(y)
+
+z=text.find('until')
+print(z)
+
+a=text.isalpha()
+print(a)
+
+b=text.isalnum()
+print(b)
+
+
+
+
+
+
